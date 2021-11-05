e87dbddb66f19f582949a2c18b85e626fc195bf7
jperez1478 <jessicperez3@gmail.com>
2021-11-05 01:19:36 -0500

    Initial Commit

---
+++ audio/audioApp.swift
@@ -0,0 +1,17 @@
+//
+//  audioApp.swift
+//  audio
+//
+//  Created by Jessica Perez on 11/5/21.
+//
+
+import SwiftUI
+
+@main
+struct audioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
